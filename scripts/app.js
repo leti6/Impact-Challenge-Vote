c90438aff881bf55f6f98c8261ec04bb006eed00
MSI\Student <laura_custers@hotmail.com>
2017-11-09 14:19:22 +0100

    mise à jour de toutes les pages en style

--- scripts/app.js
+++ scripts/app.js
@@ -5,8 +5,61 @@ function initSlick(){
     $('.autoplay').slick({
         infinite: true,
         slidesToShow: 3,
-        slidesToScroll: 1,
+        slidesToScroll: 2,
         autoplay: true,
-        autoplaySpeed: 3000
+        autoplaySpeed: 2500
       });
 }
+
+//  var myIndex = 0;
+//  carousel();
+
+
+// function carousel() {
+//     var i;
+//  var x = document.getElementsByClassName("mySlides");
+//     for (i = 0; i < x.length; i++) {
+//        x[i].style.display = "none";  
+//      }
+//      myIndex++;
+//      if (myIndex > x.length) {myIndex = 1}    
+//     x[myIndex-1].style.display = "block";  
+//      setTimeout(carousel, 3000);    
+//  }
+
+
+
+
+// Get titles from the DOM
+var titleMain  = $("#animatedHeading");
+var titleSubs  = titleMain.find("slick-active");
+
+if (titleMain.length) {
+
+  titleMain.slick({
+    autoplay: false,
+    arrows: false,
+    dots: false,
+    slidesToShow: 3,
+    centerPadding: "10px",
+    draggable: false,
+    infinite: true,
+    pauseOnHover: false,
+    swipe: false,
+    touchMove: false,
+    vertical: true,
+    speed: 1000,
+    autoplaySpeed: 2000,
+    useTransform: true,
+    cssEase: 'cubic-bezier(0.645, 0.045, 0.355, 1.000)',
+    adaptiveHeight: true,
+  });
+
+  // On init
+  $(".slick-dupe").each(function(index, el) {
+    $("#animatedHeading").slick('slickAdd', "<div>" + el.innerHTML + "</div>");
+  });
+
+  // Manually refresh positioning of slick
+  titleMain.slick('slickPlay');
+};
